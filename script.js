4beb2d89ab2471b938897d582a8db2274619c9fa
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-05 13:40:06 -0600

    nested loops with arrays

--- script.js
+++ script.js
@@ -16,20 +16,24 @@ const storeInventory = [["Item: Computer", "Price: $1500", "Inventory: $25"],
 ["Item: Album", "Price: $15", "Inventory: 95"]
 ];
 
-for(let i = 0; i < storeInventory.length; i++) {
-    // this returns an array, but I dont specifically want an array here
-    // so I just converted to a string so which could be pushed to a div or section
-    // to create an automatic lineup of products for example.
-    let newStr = storeInventory[i].toString();
-    console.log(newStr);
-}
+// for(let i = 0; i < storeInventory.length; i++) {
+//     // this returns an array, but I dont specifically want an array here
+//     // so I just converted to a string so which could be pushed to a div or section
+//     // to create an automatic lineup of products for example.
+//     let newStr = storeInventory[i].toString();
+//     console.log(newStr);
+// }
 
 // this is another practical example, so the array items can be edited further down the line
 // making them more dynamic
 
 for(let i = 0; i < storeInventory.length; i++) {
-    // this returns an array, but I dont specifically want an array here
-    // so using an additional loop I will convert all these to strings
-    let newStr = storeInventory[i].toString();
-    console.log(newStr);
+    if(i > 0) {
+        console.log(".....................................................")
+    }
+    for(let j = 0; j < storeInventory[i].length; j++) {
+        console.log(storeInventory[i][j]);
+    }
 }
+
+// the above loop is essentially counting    1: 1,2,3,    2: 1,2,3,    3: 1,2,3   etc.....
