f3938513c9e11c636e17734a23c7612bff658381
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-05 13:17:59 -0600

    nested for loops practice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("JS Linked")
+for(let i = 0; i < 11; i++) {
+    for(let j = 0; j < 11; j++) {
+        console.log(` ${i} * ${j} = ${i * j}`)
+    }
+    console.log(`This is your ${i} time running the loop`)
+}
+
